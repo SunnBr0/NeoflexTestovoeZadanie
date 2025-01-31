647e97e52986ea5ce81262eb96718aef1f86df5d
SunnBr0 <vova.voronov.2002@bk.ru>
2025-02-01 01:16:49 +0300

    The navigate is ready

--- src/components/Navigate.tsx
+++ src/components/Navigate.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import "../style/Navigate.css"
 
 function Navigate() {
+  let likes: number = 0;
+  let buyValue: number = 0;
   return (
     <div className="header">
       <div className="navigate">
@@ -9,18 +11,23 @@ function Navigate() {
         <div className="containerIconFavouritesBay">
           <div className="iconFavourites">
             <img className="iconFavouritesPart" src="./assets/Favourites.svg" alt="" />
-            {/* <img className="iconCircleFavouritesPart" src="./assets/Circle.svg" alt="" /> */}
-            {/* <span className="countFavouritesPart">10</span> */}
+            {likes > 0 ?
+              <>
+                <img className="iconCircleFavouritesPart" src="./assets/Circle.svg" alt="" />
+                <span className="countFavouritesPart">{likes}</span>
+              </> : <></>
+            }
           </div>
           <div className="iconBay">
-
             <img className="iconBayPart" src="./assets/Bay.svg" alt="" />
-            {/* <img className="iconCircleBayPart" src="./assets/Circle.svg" alt="" /> */}
-            {/* <span className="countBayPart">1</span> */}
+            {buyValue > 0 ?
+              <>
+                <img className="iconCircleBayPart" src="./assets/Circle.svg" alt="" />
+                <span className="countBayPart">{buyValue}</span>
+              </> : <></>
+            }
           </div>
         </div>
-        {/* <div className="iconLike"></div>
-        <div className="iconShoppingCart"></div> */}
       </div>
     </div>
   );
